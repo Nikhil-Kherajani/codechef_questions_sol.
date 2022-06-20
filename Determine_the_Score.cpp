01cb64ef848a4a769d015bd4367ede3a43463d75
nikhilkherajani <nikhilkherajani7@gmail.com>
2022-06-20 19:25:41 +0530

    initial

---
+++ Determine_the_Score.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T , x , y , n , z ,  b , c;
+	string s;
+	
+    cin>>T;
+	while(T){
+	   
+	  cin>>x>>n;
+
+	int i = x/10;
+	 i = i*n;
+	 cout<<i<<endl; 
+
+	   T--; 
+	}
+	return 0;
+
+}
